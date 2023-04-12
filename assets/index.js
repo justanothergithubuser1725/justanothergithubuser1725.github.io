e37269e84b626d05b1901d152efba64dd58c6f8b
justanothergithubuser1725 <127348105+justanothergithubuser1725@users.noreply.github.com>
2023-04-12 15:52:34 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,22 @@
-var message = prompt("Hi.");
-while (message.toLowerCase() !== 'exit') {
-    message = prompt(message);
+"use strict";
+const canvas = document.getElementById("canvas");
+console.assert(canvas instanceof HTMLCanvasElement, "Canvas is missing");
+const context = canvas.getContext("2d",{alpha: false});
+function redraw(width,height) {
+    context.fillStyle="#000000";
+    context.fillRect(0,0,width,height);
+    context.fillStyle="#ffffff";
+    context.fillRect(100,100,100,100);
 }
+//Note: requestIdleCallback adds something that's called when idle
+requestAnimationFrame(function self(time) {
+    redraw(canvas.width,canvas.height);
+    requestAnimationFrame(self);
+});
+window.addEventListener('resize',(event)=>{
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+    redraw(width,height);
+});
