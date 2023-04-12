061322438aae5daef0137a1d0a5d228db0b0bce8
justanothergithubuser1725 <127348105+justanothergithubuser1725@users.noreply.github.com>
2023-04-12 15:51:52 -0400

    Create proxy.js

---
+++ useless/proxy.js
@@ -0,0 +1,44 @@
+"use strict";
+new Proxy({__proto__: null,
+
+},{__proto__: null,
+    get (target, name, receiver) {
+        
+    },
+    set (target, name, value, receiver) {
+
+    },
+    has (target, name) {
+
+    },
+    apply (target, thisArg, args) {
+
+    },
+    construct (target, args, newTarget) {
+
+    },
+    defineProperty (target, name, descriptor) {
+
+    },
+    getOwnPropertyDescriptor (target, name) {
+
+    },
+    deleteProperty (target, name) {
+
+    },
+    getPrototypeOf (target) {
+
+    },
+    setPrototypeOf (target, value) {
+
+    },
+    ownKeys (target) {
+
+    },
+    isExtensible (target) {
+        
+    },
+    preventExtensions (target) {
+
+    },
+});
